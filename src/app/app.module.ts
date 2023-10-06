22b4719594533c6f5b141bcbfd7b1046b7686d7e
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2023-10-05 23:42:27 -0500

    Se agrega la transición al componente login y register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { routing } from "./app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent],
+  imports: [FormsModule, MatDialogModule,  BrowserModule, routing],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
