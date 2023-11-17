c5f61e1e05abbb45baf10231d8093db5f9956321
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2023-11-16 21:15:17 -0500

    Se obtiene el usurio desde el perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
+import axios from "axios";
 
 @Component({
   selector: 'app-perfil',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  readonly APIUrl="http://localhost/";
+  usuario=null;
+
+  constructor(private cookieService: CookieService){}
   
+  
+  obtenerUsuario(){
+
+
+    axios.post( this.APIUrl+'iniciarsesion',
+      {
+        token: this.cookieService.get('token');
+      }
+    )
+    .then(
+      (res) => {
+          if(res && res.data){
+            
+          }else {
+            
+          }
+      })
+      .catch((error) =>{
+        alert(error);
+      });  
+  }
 }
